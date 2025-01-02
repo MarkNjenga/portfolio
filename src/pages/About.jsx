fe2e31dfc0a4e9fb76181017781a488bed03a6d8
Mark Njenga <marknjenga25@gmail.com>
2025-01-02 08:34:57 +0300

    Fixed the broken images

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const About = () => {
-    const imageUrl = '/public/assets/WhatsApp Image 2024-12-26 at 17.44.04.jpeg';
+    const imageUrl = '/assets/WhatsApp Image 2024-12-26 at 17.44.04.jpeg';
     return ( 
         <div id='about' className="about">
             <h1 className="about__title">About me</h1>
@@ -24,11 +24,11 @@ const About = () => {
                     <div className="technologies">
                         <h3 className="technologies-title">Technologies</h3>
                         <div className="tech-icons">
-                            <img src="/public/assets/png-clipart-javascript-logo-computer-icons-vue-js-angle-text-removebg-preview.png" alt="Javascript logo" />
-                            <img src="/public/assets/react-removebg-preview.png" alt="React logo" />
-                            <img src="/public/assets/python-logo-removebg-preview.png" alt="Python logo" />
-                            <img src="/public/assets/django-framework1720881581.logowik.com-Photoroom.png" alt="Django logo" />
-                            <img src="/public/assets/flask-icon-removebg-preview.png" alt="Flask Logo" />
+                            <img src="/assets/png-clipart-javascript-logo-computer-icons-vue-js-angle-text-removebg-preview.png" alt="Javascript logo" />
+                            <img src="/assets/react-removebg-preview.png" alt="React logo" />
+                            <img src="/assets/python-logo-removebg-preview.png" alt="Python logo" />
+                            <img src="/assets/django-framework1720881581.logowik.com-Photoroom.png" alt="Django logo" />
+                            <img src="/assets/flask-icon-removebg-preview.png" alt="Flask Logo" />
                         </div>
                     </div>
                 </div>
